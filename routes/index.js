c510bbe05ff0e458f74aebd4943f1c6463580c13
alexswope <awswope@gmail.com>
2018-12-22 19:52:26 -0500

    multiple users supported through session cookies

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 const readline = require('readline');
 var router = express.Router();
 var request = require('request');
+var Promise = require('promise');
+
 const fs = require('fs');
 const TOKEN_PATH = 'token.json';
 const {google} = require('googleapis');
@@ -9,6 +11,7 @@ var authUrl="test";
 var oAuth2Client;
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 var credContent;
+var sdsWinToken;
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
@@ -36,6 +39,16 @@ function authorize (credContent){
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  // Access the session as req.session
+
+    if (req.session.views) {
+      req.session.views++
+
+
+    } else {
+      req.session.views = 1
+
+    }
 
 
     res.render('index', { title: 'Express', authUrl:authUrl });
@@ -44,89 +57,99 @@ router.get('/', function(req, res, next) {
 router.get('/auth/google/callback', async function(req, res, next)
 {
 
-  // // Load client secrets from a local file.
-  // fs.readFile('credentials.json', (err, content) => {
-  //   if (err) return console.log('Error loading client secret file:', err);
-  //   // Authorize a client with credentials, then call the Google Drive API.
-  //   authorize(JSON.parse(content), listFiles);
-  // });
 
-  /**
-   * Create an OAuth2 client with the given credentials, and then execute the
-   * given callback function.
-   * @param {Object} credentials The authorization client credentials.
-   * @param {function} callback The callback to call with the authorized client.
-   */
-  // function authorize(credentials, callback) {
-    // const {client_secret, client_id, redirect_uris} = credentials.web;
-    // const oAuth2Client = new google.auth.OAuth2(
-    //     client_id, client_secret, redirect_uris[0]);
+function authorizeUser(req){
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getAccessToken(oAuth2Client, listFiles);
+      return new Promise(function(resolve, reject){
 
-      oAuth2Client.setCredentials(JSON.parse(token));
+
+        console.log('Cookies: ', req.cookies)
+    // sdsWinToken = req.cookies.sdsWinToken
+    console.log('req.session');
+    console.log(req.session);
+    if (req.session.sdsWinToken===undefined)//this seems to always be true
+    {
+      console.log('no cookie found for sdsWinToken, getting token');
+      getAccessToken(oAuth2Client, listFiles)
+
+    }
+    else {
+      console.log('sdsWinToken found, using it to set credentials:');
+      console.log(req.session.sdsWinToken);
+
+      oAuth2Client.setCredentials(req.session.sdsWinToken);
       console.log('about to call list files');
       listFiles(oAuth2Client);
-    });
 
-  /**
-   * Get and store new token after prompting for user authorization, and then
-   * execute the given callback with the authorized OAuth2 client.
-   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-   * @param {getEventsCallback} callback The callback for the authorized client.
-   */
-  function getAccessToken(oAuth2Client, callback) {
-    // const authUrl = oAuth2Client.generateAuthUrl({
-    //   access_type: 'offline',
-    //   scope: SCOPES,
-    // });
-    //console.log('Authorize this app by visiting this url:', authUrl);
-    // const rl = readline.createInterface({
-    //   input: process.stdin,
-    //   output: process.stdout,
-    // });
-    // rl.question('Enter the code from that page here: ', (code) => {
-    //   rl.close();
-    code=req.query.code;
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
+    }
+
+    function getAccessToken(oAuth2Client, callback) {
+
+      code=req.query.code;
+        oAuth2Client.getToken(code, (err, token) => {
+          if (err) return console.error('Error retrieving access token', err);
+          console.log('token:');
+          console.log(token);
+          sdsWinToken = token;
+          console.log('sdsWinToken set to token and now value is:')
+          console.log(sdsWinToken);
+          oAuth2Client.setCredentials(sdsWinToken);
+          // Store the token to disk for later program executions
+          // fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          //   if (err) console.error(err);
+          //   console.log('Token stored to', TOKEN_PATH);
+          // });
+
+          //set the users cookie for the token for multiple calls and multiple users
+          req.session.sdsWinToken = sdsWinToken;
+          console.log ("sds win token in res is" + req.session.sdsWinToken)
+          console.log("about to callback listFiles");
+          callback(oAuth2Client);
         });
 
-        console.log("about to callback listFiles");
-        callback(oAuth2Client);
+    }
+
+    function listFiles(auth) {
+      console.log('inside listFiles');
+      console.log('req.session.sdsWinToken:')
+      console.log(req.session.sdsWinToken)
+
+      const drive = google.drive({version: 'v3', auth});
+      var fileMetadata = {
+        'name': 'Invoices',
+        'mimeType': 'application/vnd.google-apps.folder'
+      };
+      drive.files.create({
+        resource: fileMetadata,
+        fields: 'id'
+      }, function (err, res) {
+        if (err) {
+          // Handle error
+          console.error(err);
+        } else {
+          console.log('Folder id: ', res.data.id);
+        }
       });
+resolve();
+}//end listfiles
 
-  }
+});//end promise
 
-  function listFiles(auth) {
-    console.log('inside listFiles');
-    const drive = google.drive({version: 'v3', auth});
-    drive.files.list({
-      pageSize: 10,
-      fields: 'nextPageToken, files(id, name)',
-    }, (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const files = res.data.files;
-      if (files.length) {
-        console.log('Files:');
-        files.map((file) => {
-          console.log(`${file.name} (${file.id})`);
-        });
-      } else {
-        console.log('No files found.');
-      }
-    });
-  }
+}//end authorizeUser
+
+
+authorizeUser(req).then(function(){
+  console.log('\n\nright before rednering win req.session.sdsWinToken:')
+console.log(req.session.sdsWinToken)
 res.render('win',{title:'Folder Created'})
 
 });
+
+
+
+}); //end router
+
+
   /**
    * Lists the names and IDs of up to 10 files.
    * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
@@ -224,8 +247,5 @@ router.get('/win', function(req, res, next) {
   // });
 });
 
-function listFiles(auth) {
-
-  }
 
 module.exports = router;
